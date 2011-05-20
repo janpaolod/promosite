8e0e62597371cf7c2851a7937df6587a80b93314
JP Dipasupil <janpaolod@gmail.com>
2011-05-20 17:54:00 +0800

    changes in the promo mailer

--- app/mailers/promo_mailer.rb
+++ app/mailers/promo_mailer.rb
@@ -1,5 +1,5 @@
 class PromoMailer < ActionMailer::Base
-  default :from => "admin@filsale.com"
+  default :from => "admin@twiggzy.com"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
