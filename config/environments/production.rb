31d90a0401eb992108c2f0f28cd1483c00b9ec30
JP Dipasupil <janpaolod@gmail.com>
2011-06-06 21:37:03 +0800

    use sendmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,10 +9,6 @@ Promosite::Application.configure do
   ### ActionMailer Config
   ## Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :sendmail
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default :charset => "utf-8"
-  
   config.action_mailer.sendmail_settings = {
      :location =&gt; '/usr/sbin/sendmail',
      :arguments =&gt; '-i -t'
